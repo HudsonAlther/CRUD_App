2b159ff77cd684514ea5c76b59668a3c38b69c61
HudsonAlther <dvu7jh@virginia.edu>
2024-12-04 19:49:14 -0500

    basic setup

--- src/main/java/edu/virginia/sde/controller/MyReviewsController.java
+++ src/main/java/edu/virginia/sde/controller/MyReviewsController.java
@@ -14,7 +14,6 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.List;
 
